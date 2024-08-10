d3c0eacaa56a2f675fc0a4cc88d65528323f60eb
syahridho arjuna syahputra <syahridhosyahputra@gmail.com>
2024-08-11 02:04:25 +0700

    fix forecasts if null

--- src/App.tsx
+++ src/App.tsx
@@ -106,24 +106,22 @@ const App = () => {
       )}
 
       <div className="flex overflow-x-auto space-x-4 px-4 w-full">
-        {!isLoading ? (
-          forecasts.map((forecast: any) => (
-            <div
-              key={forecast.dt}
-              className="flex-shrink-0 border flex flex-col justify-center items-center py-2 px-1 bg-white rounded shadow"
-            >
-              <h1>{getDayName(forecast.dt)}</h1>
-              <img
-                src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`}
-                alt={forecast.dt}
-                className=""
-              />
-              <h1>{(forecast.main.feels_like - 273.15).toFixed(2)}°C</h1>
-            </div>
-          ))
-        ) : (
-          <h1>Gak ada</h1>
-        )}
+        {!isLoading
+          ? forecasts.map((forecast: any) => (
+              <div
+                key={forecast.dt}
+                className="flex-shrink-0 border flex flex-col justify-center items-center py-2 px-1 bg-white rounded shadow"
+              >
+                <h1>{getDayName(forecast.dt)}</h1>
+                <img
+                  src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`}
+                  alt={forecast.dt}
+                  className=""
+                />
+                <h1>{(forecast.main.feels_like - 273.15).toFixed(2)}°C</h1>
+              </div>
+            ))
+          : null}
       </div>
     </div>
   );
